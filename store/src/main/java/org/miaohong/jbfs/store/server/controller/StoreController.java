ccc570ec5c2fa57eada61c096e61094db774c18d
oikomi <miaohonghit@gmail.com>
2016-01-25 10:12:25 +0800

    init

--- store/src/main/java/org/miaohong/jbfs/store/server/controller/StoreController.java
+++ store/src/main/java/org/miaohong/jbfs/store/server/controller/StoreController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -25,9 +27,27 @@ public class StoreController {
         store = Store.getInstance();
     }
 
+//    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+//    public void upload(@RequestParam("vid") String vid, @RequestParam("key") String key,
+//                       @RequestParam("cookie") String cookie, @RequestParam("file") MultipartFile file) {
+//        System.out.println("upload");
+//        try {
+//            store.upload(Integer.parseInt(vid), Integer.parseInt(key), cookie, file.getSize(), file.getBytes());
+//        } catch (StoreAdminException e) {
+//            e.printStackTrace();
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public void upload(@RequestParam("vid") String vid, @RequestParam("key") String key,
-                       @RequestParam("cookie") String cookie, @RequestParam("file") MultipartFile file) {
+    public void upload(@RequestParam("file") MultipartFile file, HttpServletRequest req, HttpServletResponse resp) {
+        String vid = req.getParameter("vid");
+        String key = req.getParameter("key");
+        String cookie = req.getParameter("cookie");
+
         System.out.println("upload");
         try {
             store.upload(Integer.parseInt(vid), Integer.parseInt(key), cookie, file.getSize(), file.getBytes());
