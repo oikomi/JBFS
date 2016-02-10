d9d4f4ec99dd3d705d6e2748b0db29d9487a759a
oikomi <miaohonghit@gmail.com>
2016-02-10 19:51:30 +0800

    init

--- pitchfork/src/main/java/org/miaohong/jbfs/pitchfork/server/Pitchfork.java
+++ pitchfork/src/main/java/org/miaohong/jbfs/pitchfork/server/Pitchfork.java
@@ -9,4 +9,23 @@ public class Pitchfork {
 
     private PitchforkConfig pitchforkConfig = PitchforkConfig.getInstance();
 
+    private Pitchfork() {}
+
+
+    private static class SingletonHolder {
+        private static final Pitchfork INSTANCE = new Pitchfork();
+    }
+
+    public static final Pitchfork getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    public void run() {
+
+        
+    }
+
+//    public static void main(String[] args) {
+//
+//    }
 }
---
+++ pitchfork/src/main/java/org/miaohong/jbfs/pitchfork/server/PitchforkDaemon.java
@@ -0,0 +1,14 @@
+package org.miaohong.jbfs.pitchfork.server;
+
+/**
+ * Created by miaohong on 16/2/10.
+ */
+public class PitchforkDaemon {
+
+
+    public static void main(String[] args) {
+        Pitchfork pitchfork = Pitchfork.getInstance();
+
+        pitchfork.run();
+    }
+}
